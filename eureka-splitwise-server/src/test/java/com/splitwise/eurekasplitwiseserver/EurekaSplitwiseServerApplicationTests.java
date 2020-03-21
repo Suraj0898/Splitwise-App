98b91de5e4b48cf09192c48e5038462af22fe0c2
Suraj Suhas <sursuhas@publicisgroupe.net>
2020-03-21 20:51:33 +0530

    Second commit

---
+++ eureka-splitwise-server/src/test/java/com/splitwise/eurekasplitwiseserver/EurekaSplitwiseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitwise.eurekasplitwiseserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSplitwiseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
